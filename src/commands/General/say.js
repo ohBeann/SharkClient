959ebaa1b05413795f5219bd70869ff9acc1d432
Sean McLennan <spmclennan@yahoo.com>
2021-11-21 12:13:45 -0800

    src: inital commit

---
+++ src/commands/General/say.js
@@ -0,0 +1,22 @@
+const { Util } = require('discord.js');
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            enabled: true,
+            description: 'Says what you tell it to',
+            extendedHelp: 'No extended help available.',
+            usage: '',
+        });
+    }
+
+    async run(message) {
+        const argument = message.args.join(' ')
+        const say = Util.removeMentions(argument)
+        message.delete()
+        message.channel.send(say)
+    }
+
+};
