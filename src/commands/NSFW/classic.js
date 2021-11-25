3fa7dfc7e779e8663c63dd3f3357c32f5f85744d
Sean McLennan <spmclennan@yahoo.com>
2021-11-25 11:13:57 -0800

    src: inital commit

---
+++ src/commands/NSFW/classic.js
@@ -0,0 +1,28 @@
+const {
+    Command
+} = require('klasa');
+const Discord = require('discord.js')
+const _Client = require('nekos.life')
+const neko = new _Client;
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            enabled: true,
+            runIn: ['text', 'dm', 'group'],
+            deletable: false,
+            nsfw: true,
+            permissionLevel: 0,
+            extendedHelp: 'No extended help available.',
+        });
+    }
+
+    async run(message) {
+        if(message.guild.settings.nsfw == false) throw `Guild does not have NSFW enabled!`
+        const embed = new Discord.MessageEmbed()
+            .setImage(await (await neko.nsfw.classic()).url)
+            .setFooter('Powered by nekos.life')
+        message.channel.send(embed)
+    }
+};
