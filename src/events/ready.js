bcacf41f104c8a6450eb56218f6f2482b94bed9b
Sean McLennan <spmclennan@yahoo.com>
2021-11-27 12:50:29 -0800

    misc: once is now set to false

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,6 +6,7 @@ module.exports = class extends Event {
 
         super(...args, {
             enabled: true,
+            once: false
         });
     }
 
@@ -18,3 +19,4 @@ module.exports = class extends Event {
     }
 
 };
+
