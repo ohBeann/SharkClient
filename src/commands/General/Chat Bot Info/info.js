786a4aac8c8b201aa9377063f71a7478819fcb31
Sean McLennan <spmclennan@yahoo.com>
2021-11-27 13:06:08 -0800

    specified who can use the conf command

--- src/commands/General/Chat Bot Info/info.js
+++ src/commands/General/Chat Bot Info/info.js
@@ -17,7 +17,7 @@ module.exports = class extends Command {
 			.setAuthor('───── Shark Bot#5422 ─────')
 			.setThumbnail(this.client.user.displayAvatarURL())
 			.setColor("BLUE")
-			.setDescription(`》To get started, run \`${guildPrefix}help\` to see a list of commands that you can use. \n 》You can @mention me to get my prefix too! \n 》Running \`${guildPrefix}prefix\` will show you the current prefix for the server. Change it with \`${guildPrefix}prefix <prefix>\` \n 》Unlock the full experience of the bot by setting it up with the command \`${guildPrefix}conf show\` \n \n A Discord bot created by **Bean.#0002** \n \n With love, \n - Bean`)
+			.setDescription(`》To get started, run \`${guildPrefix}help\` to see a list of commands that you can use. \n 》You can @mention me to get my prefix too! \n 》Running \`${guildPrefix}prefix\` will show you the current prefix for the server. Change it with \`${guildPrefix}prefix <prefix>\` \n 》Unlock the full experience of the bot by setting it up with the command \`${guildPrefix}conf show\` (mods only) \n \n A Discord bot created by **Bean.#0002** \n \n With love, \n - Bean`)
 			.setFooter('Support server soon™️')
 
 		message.send(embed)
