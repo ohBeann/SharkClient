4f1437b05ed9f7ce47caf3b01a3b43c670b480c6
Sean McLennan <spmclennan@yahoo.com>
2021-11-27 15:15:29 -0800

    runs in init function now

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,20 +3,17 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 
     constructor(...args) {
-
         super(...args, {
             enabled: true,
             once: false
         });
     }
 
-    async run(...params) {
+    async run(message) {
+        
+    }
+    async init(){
         this.client.user.setActivity(`over ${this.client.users.cache.size.toLocaleString()} users | .help`, { type: 'WATCHING'})
     }
 
-    async init() {
-
     }
-
-};
-
