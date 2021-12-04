0848a7d5cd4d80bf796b5abccda259db48642166
Sean McLennan <spmclennan@yahoo.com>
2021-12-03 16:34:13 -0800

    permission fix

--- src/lib/util/permissionLevels.js
+++ src/lib/util/permissionLevels.js
@@ -2,4 +2,4 @@ const { Client, PermissionLevels} = require('klasa')
 
 Client.defaultPermissionLevels
     .add(5, ({ guild, member }) => guild && member.roles.cache.has(guild.settings.get("roles.modrole")) || member.permissions.has('BAN_MEMBERS'), { fetch: true})
-    .add(4, ({ guild, member}) => guild && member.roles.cache.has("916480986097799248"), {break: true}) || member.permissions.has("MANAGE_GUILD")
+    .add(4, ({ guild, member}) => guild && member.roles.cache.has("916480986097799248"), {break: true}) || member.hasAtLeastPermissionLevel(6)
