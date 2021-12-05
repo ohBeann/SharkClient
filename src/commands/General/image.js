5d70ffa2161f344fa6ca18551219a67aabb95f4d
Sean McLennan <spmclennan@yahoo.com>
2021-12-05 09:50:36 -0800

    disabled due to nsfw issues

--- src/commands/General/image.js
+++ src/commands/General/image.js
@@ -6,7 +6,7 @@ module.exports = class extends Command {
 
     constructor(...args) {
         super(...args, {
-            enabled: true,
+            enabled: false,
             runIn: ['text'],
             aliases: ['img'],
             description: 'Scrapes an image off google',
@@ -14,25 +14,56 @@ module.exports = class extends Command {
         });
     }
 
-   async run(message, args) {
-    var Scraper = require('images-scraper');
-    const google = new Scraper({
-        puppeteer: {
-            headless: true
-        },
-    });
-    const Discord = require('discord.js');
-    const image_query = message.args.join(" ");
+    async run(message, args) {
+        var Scraper = require('images-scraper');
+        const google = new Scraper({
+            puppeteer: {
+                headless: true
+            },
+        });
+        const Discord = require('discord.js');
+        let image_query = message.args.join(" ");
 
-    const image_results = await google.scrape(image_query, 10);
+        if (!message.guild.settings.get("nsfw", false)) {
+            if (message.args === "porn",
+                "hentai",
+                "nsfw",
+                "pussy",
+                "ass",
+                "dick",
+                "deep throat",
+                "deepthroat",
+                "doggystyle",
+                "doggystyle",
+                "cum",
+                "face sit",
+                "facesit",
+                "milf",
+                "upskirt",
+                "yuri",
+                "nude",
+                "nood",
+                "nudity",
+                "naked",
+                "18",
+                "ero",
+                "masturbate",
+                "masturbation",
+                "masterbate",
+                "masterbation",
+                "sex",
+                "strip",
+                "smut") return message.channel.send("You cannot search for this with NSFW disabled!")
+        }
+        const image_results = await google.scrape(image_query, 10);
 
-    const img = image_results[Math.floor(Math.random() * image_results.length)];
+        const img = image_results[Math.floor(Math.random() * image_results.length)];
 
-    const embed = new Discord.MessageEmbed()
-        .setColor("RANDOM")
-        .setImage(img.url)
+        const embed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setImage(img.url)
 
-    message.channel.send(embed)
+        message.channel.send(embed)
     }
 
 };
