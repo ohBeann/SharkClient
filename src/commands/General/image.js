1563c5691e67e73bd179d51d0810100bff78f99b
Sean McLennan <spmclennan@yahoo.com>
2021-12-05 11:11:45 -0800

    nsfw bug fixed

--- src/commands/General/image.js
+++ src/commands/General/image.js
@@ -6,7 +6,7 @@ module.exports = class extends Command {
 
     constructor(...args) {
         super(...args, {
-            enabled: false,
+            enabled: true,
             runIn: ['text'],
             aliases: ['img'],
             description: 'Scrapes an image off google',
@@ -14,7 +14,7 @@ module.exports = class extends Command {
         });
     }
 
-    async run(message, args) {
+    async run(message) {
         var Scraper = require('images-scraper');
         const google = new Scraper({
             puppeteer: {
@@ -22,40 +22,58 @@ module.exports = class extends Command {
             },
         });
         const Discord = require('discord.js');
-        let image_query = message.args.join(" ");
+        let image_query = message.args.join(" ")
+        let bannedWords = [
+            "porn",
+            "hentai",
+            "nsfw",
+            "pussy",
+            "ass",
+            "dick",
+            "deep throat",
+            "deepthroat",
+            "doggystyle",
+            "doggystyle",
+            "cum",
+            "face sit",
+            "facesit",
+            "milf",
+            "upskirt",
+            "yuri",
+            "nude",
+            "nood",
+            "nudity",
+            "naked",
+            "18",
+            "ero",
+            "masturbate",
+            "masturbation",
+            "masterbate",
+            "masterbation",
+            "sex",
+            "strip",
+            "smut",
+            "squirt",
+            "squirting"
+        ]
 
-        if (!message.guild.settings.get("nsfw", false)) {
-            if (message.args === "porn",
-                "hentai",
-                "nsfw",
-                "pussy",
-                "ass",
-                "dick",
-                "deep throat",
-                "deepthroat",
-                "doggystyle",
-                "doggystyle",
-                "cum",
-                "face sit",
-                "facesit",
-                "milf",
-                "upskirt",
-                "yuri",
-                "nude",
-                "nood",
-                "nudity",
-                "naked",
-                "18",
-                "ero",
-                "masturbate",
-                "masturbation",
-                "masterbate",
-                "masterbation",
-                "sex",
-                "strip",
-                "smut") return message.channel.send("You cannot search for this with NSFW disabled!")
+        if (!message.guild.settings.get("nsfw", true)) {
+            for (const item of bannedWords){
+                if(image_query.toLowerCase().indexOf(item) !== -1){
+                   return message.channel.send("You cannot search for this with NSFW disabled!")
+                }
+            }
         }
-        const image_results = await google.scrape(image_query, 10);
+
+        if(message.guild.settings.get("nsfw", true)){
+            for (const item of bannedWords){
+                if(image_query.toLowerCase().indexOf(item) !== -1){
+                    if(!message.channel.nsfw) return message.channel.send("You may search for this only in NSFW channels")
+                } 
+            }
+        }
+
+        const image_results = await google.scrape(image_query, 100);
 
         const img = image_results[Math.floor(Math.random() * image_results.length)];
 
