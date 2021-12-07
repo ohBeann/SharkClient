07bb26427ae465d520814f388df1696d762583f4
Sean McLennan <spmclennan@yahoo.com>
2021-12-06 21:11:46 -0800

    cleaned up schemas

--- src/lib/schemas/defaultGuildSchema.js
+++ src/lib/schemas/defaultGuildSchema.js
@@ -1,15 +1,27 @@
 const { KlasaClient } = require("klasa");
 
 KlasaClient.defaultGuildSchema
+
+	// Customs
 	.add('customs', 'any', { default: [], array: true })
+
+	// Roles
 	.add('roles', folder => folder
 		.add('muted', 'role', { default: null})
 		.add('modrole', 'role', { default: null})
 		.add('autorole', 'role', { default: null}))
+
+	// Raidmode
 	.add('raidmode', 'Boolean', { default: false})
+
+	// Channels
 	.add('channels', folder => folder
 		.add('suggestionChannel', 'channel', { default: false }))
-		.add('nsfw', 'Boolean', { default: false})
+
+	// NSFW
+	.add('nsfw', 'Boolean', { default: false})
+
+
+	// Toggles
 	.add('toggles', folder => folder
 		.add('autorole', 'Boolean', { default: false}))
-		
